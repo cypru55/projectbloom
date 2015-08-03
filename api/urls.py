60de61b328c1d251a39828dc574886ea047eb7c2
Liu Tuo <liutuo098@gmail.com>
2015-08-03 15:39:00 +0800

    url again~

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^sales_pivot_table', views.sales_pivot_table, name='sales_pivot'),
     url(r'^$', views.index, name='index'),
+    url(r'^sales_pivot_table', views.sales_pivot_table, name='sales_pivot'),
+
 ]
