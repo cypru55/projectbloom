6bf20d5a9d847a10d2a8c947716171b4d0e0f4ce
Liu Tuo <liutuo098@gmail.com>
2015-08-03 15:38:47 +0800

    url again~

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
+
 from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
+	url(r'^sales_pivot_table', views.sales_pivot_table, name='sales_pivot'),
     url(r'^$', views.index, name='index'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# root url for api, testing purpose.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the api index.")
+
+# sales pivot table data
+def sales_pivot_table(request):
+	return HttpResponse("sales api url, TODO.")
