91a597d46769c03fe7e14c2ddad538aa9afab307
Liu Tuo <liutuo098@gmail.com>
2015-08-17 15:05:35 +0800

    table pagination done

--- dashboard/static/dashboard/js/controllers.js
+++ dashboard/static/dashboard/js/controllers.js
@@ -11,61 +11,92 @@ var response_data = {}
 var dashboardControllers = angular.module('dashboardControllers', []);
 
 dashboardControllers.controller('DashboardOverviewCtrl', ['$scope', '$http',
-	function($scope, $http) {
+	function ($scope, $http) {
 		console.log('a')
 	}
 ]);
 
 dashboardControllers.controller('DashboardTableCtrl', ['$scope', '$routeParams', '$http',
-	function($scope, $routeParams, $http) {
+	function ($scope, $routeParams, $http) {
 		// Set title
+		var url;
 		if ($routeParams.table_type == 'sale') {
 			$scope.title = 'Sale Table'
+			url = '../api/' + $routeParams.table_type;
 		} else if ($routeParams.table_type == 'product_margin') {
 			$scope.title = 'Product Margin Table'
+			url = '../api/' + $routeParams.table_type + '/latest';
 		} else if ($routeParams.table_type == 'delivery') {
 			$scope.title = 'Delivery Table'
+			url = '../api/' + $routeParams.table_type;
 		}
 
-		// retrive data
-		$http.get('../api/' + $routeParams.table_type).success(function(data) {
-			header_structure = [];
-			for (var k in data.results[0]) {
-				header_structure.push({
-					"data": k
-				});
-			}
-			$scope.headers = header_structure;
-			response_data = data;
-			$('#dataTable').DataTable({
-				"scrollX": true,
-				data: response_data.results,
-				columns: header_structure
-			});
-		});
-		// $scope.tableRenderCompleted = function() {
-		// 	$('#dataTable').DataTable({
-		// 		data: response_data.results,
-		// 		columns: header_structure
-		// 	});
-		// }
+		// setup paginator
+		var options = {
+			bootstrapMajorVersion: 3,
+			useBootstrapTooltip: true,
+			onPageClicked: function (e, originalEvent, type, page) {
+                retriveTableData(url, page, 10, $http, $scope, $('#data-table-paginator'));
+            },
+			itemContainerClass: function (type, page, current) {
+                return (page === current) ? "active" : "pointer-cursor";
+            }
+		}
+
+		$('#data-table-paginator').bootstrapPaginator(options);
+		retriveTableData(url, 1, 10, $http, $scope, $('#data-table-paginator'))
+
 	}
 ]);
 
 dashboardControllers.controller('DashboardChartCtrl', ['$scope', '$http',
-	function($scope, $http) {
+	function ($scope, $http) {
 		console.log('c')
 	}
 ]);
 
 dashboardControllers.controller('DashboardFormCtrl', ['$scope', '$http',
-	function($scope, $http) {
+	function ($scope, $http) {
 		console.log('d')
 	}
 ]);
 
 dashboardControllers.controller('DashboardExportCtrl', ['$scope', '$http',
-	function($scope, $http) {
+	function ($scope, $http) {
 		console.log('e')
 	}
 ]);
+
+function retriveTableData(url, page_num, page_size, $http, $scope, table) {
+	// add page number to http get parameter
+	url = url + '?page=' + page_num;
+
+	// retrive data
+	$http.get(url).success(function (data) {
+		header_structure = [];
+		for (var k in data.results[0]) {
+			header_structure.push({
+				"data": k
+			});
+		}
+		
+		// update page size and total page
+		var newOption = {
+			totalPages: Math.ceil(data.count / page_size),
+		}
+		table.bootstrapPaginator(newOption);
+				
+		// update view
+		$scope.headers = header_structure;
+		$scope.sales = data.results;
+		$scope.z = data.count;
+		$scope.x = (page_num - 1) * page_size + 1;
+		if (page_num * page_size < data.count) {
+			$scope.y = page_num * page_size;
+		}
+		else {
+			$scope.y = data.count;
+		}
+
+	});
+}
