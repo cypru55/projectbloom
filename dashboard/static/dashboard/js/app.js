6a782f3fc5126b93edbe3c0bf2c3304aa80bbfe6
Liu Tuo <liutuo098@gmail.com>
2015-08-24 10:15:26 +0800

    try a new js chart plugin

--- dashboard/static/dashboard/js/app.js
+++ dashboard/static/dashboard/js/app.js
@@ -1,13 +1,14 @@
 /* 
 * @Author: archer
 * @Date:   2015-08-12 17:52:19
-* @Last Modified 2015-08-14
+* @Last Modified 2015-08-24
 */
 
 'use strict';
 
 var dashboardApp = angular.module('dashboardApp', [
   'ngRoute',
+  'n3-line-chart',
   'dashboardControllers',
 ]);
 
